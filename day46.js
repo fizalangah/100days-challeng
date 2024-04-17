6503957120ab31ef60a0a83d4ecd66c3068e2b5f
fiza langah <160244285+fizalangah@users.noreply.github.com>
2024-04-17 17:48:53 +0500

    Add files via upload

---
+++ day46.js
@@ -0,0 +1,17 @@
+// day 46 challenge;
+// q136;
+// for (let i = 1; i <= 5; i++) {
+//     console.log(`Iteration ${i}, i value:, i`);
+// }
+// q137;
+try {
+    throw new Error("something went wrong");
+}
+catch (error) {
+    console.log(error.message);
+}
+// q138;
+console.log("Before breakpoint");
+// Set a breakpoint on the following line using your browser's developer tools
+console.log("This line has a breakpoint set on it in the browser's developer tools");
+console.log("After breakpoint");
