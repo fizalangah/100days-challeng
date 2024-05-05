408d4002ba3d353da1811647f27ffadad0b285f3
fiza langah <160244285+fizalangah@users.noreply.github.com>
2024-05-04 23:22:17 -0700

    Add files via upload

---
+++ day55.js
@@ -0,0 +1,24 @@
+"use strict";
+// DATA TYPES 
+//NUMBERS:
+let num = 9; // output = 9
+console.log(num);
+//srtings
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+console.log(str);
+console.log(str2);
+console.log(phrase);
+// boolean;
+let nameFieldChecked = true; // yes, name field is checked
+let ageFieldChecked = false; // no, age field is not checked
+console.log(nameFieldChecked);
+console.log(ageFieldChecked);
+//null
+let age = null;
+console.log(age);
+//undefined;
+let age1;
+console.log(age1);
+// shows "undefined"
