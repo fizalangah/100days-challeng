e857f93d1f1230c00f5e056b7ea3119de9ff56a6
fiza langah <160244285+fizalangah@users.noreply.github.com>
2024-05-07 14:01:52 +0500

    Add files via upload

---
+++ day60.js
@@ -0,0 +1,14 @@
+// functions;
+ function  printmessage() {
+     console.log("hello every one")
+ }
+
+ printmessage()
+
+ /// arro function;
+
+let ptintName = (name) =>{
+    return name
+}
+
+console.log(ptintName("fiza"))
